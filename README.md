e0e95e22a4ad2ab8f59e2abfdef7ab581158a2df
Yayodelariva <eduardodlrp@outlook.com>
2025-02-10 18:06:26 -0600

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# webpack-template
+This app uses BFS (Breadth-First Seatch) in order to traverse a 8x8 chess board as a Knight. Usage is knightMoves(startposition, targetposition). The console will print the shortest possible route(s)
--- src/index.js
+++ src/index.js
@@ -1,2 +1,46 @@
 import "./style.css";
-alert("hello world");
+
+function knightMoves(start, end) {
+  const knightMoves = [
+    [2, 1],
+    [2, -1],
+    [-2, 1],
+    [-2, -1],
+    [1, 2],
+    [1, -2],
+    [-1, 2],
+    [-1, -2],
+  ];
+
+  function isValidMove(x, y) {
+    return x >= 0 && x < 8 && y >= 0 && y < 8;
+  }
+
+  let queue = [[start[0], start[1], [start]]];
+  let visited = new Set();
+  visited.add(start.toString());
+
+  while (queue.length > 0) {
+    let [x, y, path] = queue.shift();
+
+    if (x === end[0] && y === end[1]) {
+      console.log(`You made it in ${path.length - 1} moves! Here's your path:`);
+      path.forEach((step) => console.log(step));
+      return path;
+    }
+
+    for (let [dx, dy] of knightMoves) {
+      let nx = x + dx;
+      let ny = y + dy;
+
+      if (isValidMove(nx, ny) && !visited.has([nx, ny].toString())) {
+        visited.add([nx, ny].toString());
+        queue.push([nx, ny, [...path, [nx, ny]]]);
+      }
+    }
+  }
+
+  return [];
+}
+
+knightMoves([0, 0], [0, 1]); // Example
